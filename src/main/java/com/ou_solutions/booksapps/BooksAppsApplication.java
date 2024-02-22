beaf2fe0668a678cb6c04a0de110dc848196a331
Omkar Upadhyay <omkarmh009@gmail.com>
2024-02-22 17:21:58 +0800

    Initial Project source created

---
+++ src/main/java/com/ou_solutions/booksapps/BooksAppsApplication.java
@@ -0,0 +1,13 @@
+package com.ou_solutions.booksapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksAppsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ou_solutions/booksapps/BooksAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ou_solutions.booksapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksAppsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
